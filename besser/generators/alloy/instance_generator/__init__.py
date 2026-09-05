58bac2faf38ae58dbf52b6993661f1b521f368b9
marcelo <uva@dc.exa.unrc.edu.ar>
2026-09-05 05:42:58 -0300

    refactor: extract alloy_converter from alloy_solver

--- besser/generators/alloy/instance_generator/__init__.py
+++ besser/generators/alloy/instance_generator/__init__.py
@@ -1,9 +1,8 @@
-from .alloy_solver import (
-    AlloySolver,
+from .alloy_solver import AlloySolver, run_alloy_sat_validation
+from .alloy_converter import (
     AlloyToBesserConverter,
     BUMLModelIntegrator,
     alloy_xml_to_frontend_object_model,
-    run_alloy_sat_validation,
 )
 from .alloy_solver_utils import resolve_first_instance_xml
 
