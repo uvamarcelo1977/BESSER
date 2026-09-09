598335b124fac3e790bfff9a9a3f1c5347460d23
marcelo <uva@dc.exa.unrc.edu.ar>
2026-09-09 12:02:45 -0300

    refactor: mensajes

--- besser/generators/alloy/instance_generator/__init__.py
+++ besser/generators/alloy/instance_generator/__init__.py
@@ -4,12 +4,13 @@ from .alloy_converter import (
     alloy_xml_to_frontend_object_model,
 )
 from .alloy_solver import AlloySolver
-from .alloy_solver_utils import resolve_first_instance_xml
+from .alloy_solver_utils import build_error_response, resolve_first_instance_xml
 
 __all__ = [
     "AlloySolver",
     "AlloyToBesserConverter",
     "BUMLModelIntegrator",
     "alloy_xml_to_frontend_object_model",
+    "build_error_response",
     "resolve_first_instance_xml",
 ]
